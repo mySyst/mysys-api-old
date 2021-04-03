3ce2cae41a7847a047cd18bfa7e4a1e7af98bce8
brunocesarangst <bruno.angst@rede.ulbra.br>
2021-04-03 15:00:55 -0300

    database

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,24 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import express, { Application } from 'express';
-import { DemandsController } from './controllers/demads';
+import { DemandsController } from './controllers/demands';
+
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.SetupExpress();
     this.SetupControllers();
+    await this.databaseSetup();
   }
 
   private SetupExpress(): void {
     this.app.use(express.json());
+    this.SetupControllers();
   }
 
   private SetupControllers(): void {
@@ -24,5 +28,19 @@ export class SetupServer extends Server {
 
   public getApp(): Application {
     return this.app;
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.info('Server listening on port: ' + this.port);
+    });
   }
 }
