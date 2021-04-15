594d55f88c0fe7cea588ab03d68dcb775c3176ad
brunocesarangst <bruno.angst@rede.ulbra.br>
2021-04-15 13:33:57 -0300

    plus

--- src/database.ts
+++ src/database.ts
@@ -2,6 +2,7 @@ import config, { IConfig } from 'config';
 import mongoose, { Mongoose } from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
+// const mongoUrl: string | undefined = process.env.MONGODB_URL
 
 export const connect = async (): Promise<Mongoose> =>
   await mongoose.connect(dbConfig.get('mongoUrl'), {
