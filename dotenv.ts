171bea1519918313be0f8728f9ac1ebd48402f0c
brunocesarangst <bruno.angst@rede.ulbra.br>
2021-05-15 20:59:40 -0300

    fix: heroku

--- dotenv.ts
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({
-  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
-});
-
-console.log('meu env', process.env.NODE_ENV);
-console.log('meu env', process.env.PORT);
-console.log('meu env', process.env.MONGO_URL);
