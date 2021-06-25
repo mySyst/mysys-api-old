37419c88fe655c045078701bac8f3783c97bb32c
brunocesarangst <bruno.angst@rede.ulbra.br>
2021-06-24 21:07:16 -0300

    finalizando

--- src/controllers/get.ts
+++ src/controllers/get.ts
@@ -9,26 +9,25 @@ import { BaseController } from '.';
 export class GetController extends BaseController {
   @Get('get')
   public async getForLoggedUser(req: Request, res: Response): Promise<void> {
-    // const { clarify, completed } = req.query;
     const clarify = req.query.clarify;
-    const completed = req.query.completed;
+    const completed = req.query.completed == 'true';
 
     if (req.context?.userId) {
       try {
         const demands: Array<Demand> = await Demand.find({
           userId: req.context?.userId,
-          trash: false,
-          project: false,
-          completed: completed,
-          classification: clarify,
         });
-        console.log('The Inbox', demands);
-        res.status(200).send(demands);
+        const demandsFiltered = demands.filter(
+          (i) =>
+            i.classification === clarify &&
+            i.completed === completed &&
+            i.project === false &&
+            i. trash === false
+        );
+        res.status(200).send(demandsFiltered);
       } catch (error) {
         this.sendCreateUpdateErrorResponse(res, error);
       }
-    } else {
-      res.status(404).send({ code: 404, error: 'Not found' });
     }
   }
 }
