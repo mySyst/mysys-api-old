ec98522081710078c53baefdc47eec11b1b2e27c
brunocesarangst <bruno.angst@rede.ulbra.br>
2021-06-26 22:28:26 -0300

    validation title of the demand

--- src/controllers/demands.ts
+++ src/controllers/demands.ts
@@ -9,21 +9,29 @@ import { BaseController } from '.';
 export class DemandsController extends BaseController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    try {
-      const demand = new Demand({
-        ...req.body,
-        ...{
-          trash: false,
-          completed: false,
-          project: false,
-          classification: '',
-        },
-        ...{ userId: req.context?.userId },
-      });
-      const newDemand = await demand.save();
-      res.status(201).send(newDemand);
-    } catch (error) {
-      this.sendCreateUpdateErrorResponse(res, error);
+    const title = req.body.title;
+
+    if (title) {
+      try {
+        const demand = new Demand({
+          ...req.body,
+          ...{
+            trash: false,
+            completed: false,
+            project: false,
+            classification: '',
+          },
+          ...{ userId: req.context?.userId },
+        });
+        const newDemand = await demand.save();
+        res.status(201).send(newDemand);
+      } catch (error) {
+        this.sendCreateUpdateErrorResponse(res, error);
+      }
+    } else {
+      res
+        .status(203)
+        .send({ code: 203, error: 'The title is required' });
     }
   }
 }
